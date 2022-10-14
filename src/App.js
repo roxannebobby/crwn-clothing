ebbb3bde801d6eba6516befdf2ad7d8227366aec
Roxanne Weber <89050736+roxanneweber@users.noreply.github.com>
2022-10-14 11:09:56 -0500

    restructured sign in to auth

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home.component";
 import Navigation from "./routes/navigation/navigation.component.jsx";
 import Shop from "./routes/shop/shop.component";
-import SignIn from "./routes/signIn/sign-in.component.jsx";
+import Authentication from "./routes/authentication/authentication.component.jsx";
 
 const App = () => {
 	return (
@@ -11,7 +11,7 @@ const App = () => {
 			<Route path="/" element={<Navigation />}>
 				<Route index element={<Home />} />
 				<Route path="shop" element={<Shop />} />
-				<Route path="sign-in" element={<SignIn />} />
+				<Route path="auth" element={<Authentication />} />
 			</Route>
 		</Routes>
 	);
